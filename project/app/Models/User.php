297e4e8c69da937020b95fbcc38b23dd687ee0f0
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-02-25 14:21:59 +0000

    make authenticated

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',  
+
     ];
 
     /**
@@ -45,4 +47,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+  
+   
 }
